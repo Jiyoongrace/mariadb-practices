b85a8931540338e5d2c85835fc81d3fc98e40ca0
Jiyoongrace <giyun3632@naver.com>
2024-05-18 19:25:49 +0900

    :zap: fix: emaillist.sql

--- jdbc-practices/sql/emaillist.sql
+++ jdbc-practices/sql/emaillist.sql
@@ -1,7 +1,7 @@
 desc emaillist;
 
 -- (C)reate - insert
-insert into emaillist values(null, '마', '이콜', 'mychol@gmail.com');
+insert into emaillist values(null, '둘', '리', 'dooly@gmail.com');
 
 -- (R)ead - select
 select no, first_name, last_name, email from emaillist order by no desc;
