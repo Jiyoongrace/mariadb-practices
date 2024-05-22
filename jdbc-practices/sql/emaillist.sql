d46b59eb29222f361dad242728c937189c0e70b7
Jiyoongrace <giyun3632@naver.com>
2024-05-22 16:21:57 +0900

    :zap: fix: emaillist.sql

--- jdbc-practices/sql/emaillist.sql
+++ jdbc-practices/sql/emaillist.sql
@@ -1,7 +1,10 @@
 desc emaillist;
+desc guestbook;
+select * from guestbook;
+insert into guestbook values(1, "배지윤", "1234", "hi", now());
 
 -- (C)reate - insert
-insert into emaillist values(null, '둘', '리', 'dooly@gmail.com');
+insert into emaillist values(null, '지', '윤', 'jiyoon@gmail.com');
 
 -- (R)ead - select
 select no, first_name, last_name, email from emaillist order by no desc;
